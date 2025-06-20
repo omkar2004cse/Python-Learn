b92720eaaa90c959958b31c471ecb9200e963160
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-06-20 21:31:24 +0530

    Day5 code with harry

---
+++ day5.py
@@ -0,0 +1,11 @@
+# commit
+print("addition is:-",12+34) #they perform the addition
+
+
+# Escape Sequence  \n and \
+print("Hi..!\nHow are you?") #next line(\n)
+print("I am \"ok.\"\nHow are You?") #for print the cots("----" or '----' )
+
+# print extra(sep,end,file,flush)
+print("King",7,8,"Don",sep=" @@ " ,end="--",flush="fl") 
+print("\nI am ready")
