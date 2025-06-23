07bfd51b0b3a6fc74abc1f7af65907148f89e48d
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-06-23 22:48:36 +0530

    day 8 done

---
+++ day7&8.py
@@ -0,0 +1,11 @@
+# Operator 
+# Arithmatic operator (+,-,*,/,//(floor Division),%,**(exponential))
+a=12
+b=7
+print("Addition:-",a+b)
+print("substraction:-",a-b)
+print("Multiplication:-",a*b)
+print("Division:-",a/b)
+print("Reminder:-",a%b)
+print("Exponational:-",a**b)
+print("Floor Division:-",a//b) 
