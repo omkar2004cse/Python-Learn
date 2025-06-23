effb316a33ad39d45d0a9be8fcf34a0ad0af64a5
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-06-23 23:38:20 +0530

    complete

---
+++ day9.py
@@ -0,0 +1,20 @@
+# typecasting 
+# function:-int(),float(),str(),ord(),hex(),oct(),tuple(),set(),list(),dict()
+a="1234"
+print(type(a))
+a=int(a)
+print(type(a))
+
+
+# types are :-a)Explicit typecasting  and b) Implicit typecasting
+
+# Explicit typecasting 
+d=1254
+b="5828"
+# b=int(b)
+print("a+b:-",d+int(b))
+
+# Implicit typecasting(automatically conversion)
+c=12.45
+e=32
+print("e+c:-",e+c)
