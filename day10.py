6ad386daea835723cbdc1736fc8505aced01f389
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-06-24 11:58:42 +0530

    day 10

--- day10.py
+++ day10.py
@@ -4,10 +4,11 @@ print("Your name is:-",name)
 
 print("\t****Frist way****")
 a=input("input a frist number:-") # taking as input as string
-b=input("input a second number")
+b=input("input a second number:-")
 # print("Substraction is:-",x-y) these shows error because they take input as string
 print("Substraction is:-",int(a)-int(b)) # typecasting
 
+print("\t****Second way****")
 x=int(input("input a frist number:-"))
 y=int(input("input a second number:-"))
 print("multiplication of",x,"*",y,":-",x*y)
