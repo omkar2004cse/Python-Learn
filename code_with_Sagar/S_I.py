528cd8cbefa41192ba0a575faea246dd4e74ee76
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-10-16 18:01:48 +0530

    second day

---
+++ code_with_Sagar/S_I.py
@@ -0,0 +1,6 @@
+# Simple Interest 
+p=int(input("Enter a Principal Value:-"))
+r=float(input("Enter a Rate:-"))
+t=int(input("Enter a Time:-"))
+si=(p*r*t)/100
+print("Simple Interest of",p,r,t,"is:-",si)
---
+++ code_with_Sagar/variable.py
@@ -0,0 +1,14 @@
+# variable are container
+# used to store data or value
+i=12
+f=3433.50
+c=4+5j
+b=True
+s="omkar"
+l=[12,44,63,75]
+t=(12,33,53,65)
+st={12,45,74,75,12}
+d={102:"ml",103:"cl"}
+print(type(i),type(f),type(c),type(b),type(s),type(l),type(t),type(st),type(d))
+# id is inbuild function that display location of variable(identifier) stored in memory
+print(id(st))
