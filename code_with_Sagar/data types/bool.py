b704f9cf4a4fcc740ffbf2aaf1c2d799094dc274
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-02 22:41:31 +0530

    practice7 examples

---
+++ code_with_Sagar/data types/bool.py
@@ -0,0 +1,4 @@
+a=-2
+print(type(a))
+a=bool(a)
+print(type(a),a)
--- code_with_Sagar/practice/squre_root.py
+++ code_with_Sagar/practice/squre_root.py
@@ -4,3 +4,4 @@ num=int(input("Enter a Number:-"))
 # s_r=math.sqrt(num)
 s_r=num**0.5
 print("squr root of",num,"is:-",s_r)
+print(id(s_r))
