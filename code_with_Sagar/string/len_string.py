114c30ddf3985dd07ee75e18419e46858693b0fc
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-11 22:56:13 +0530

    revesion

---
+++ code_with_Sagar/string/len_string.py
@@ -0,0 +1,7 @@
+# find the entered length 
+st=input("Enter a any string:-")
+print("String is:-",st)
+count=0
+for i in st:
+    count+=1
+print("Length of string is:-",count)
--- code_with_Sagar/string/login.py
+++ code_with_Sagar/string/login.py
@@ -1,2 +1,5 @@
-pass=input("Enter a Password:-")
-if()
+pas=input("Enter a Password:-")
+if len(pas)>5 :
+    print("Login sucessful")
+else:
+    print('length is too short or <5')
