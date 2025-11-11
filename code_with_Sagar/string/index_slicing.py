29752935dc4d7af1b4718318e9c85130a509eab3
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-11 23:23:00 +0530

    slicing and indexing

---
+++ code_with_Sagar/string/index_slicing.py
@@ -0,0 +1,13 @@
+# indexing and slicing
+s=input("Enter a String:-")
+i=int(input("Enter a index number:-"))
+print('the letter is present at index',i,' is:-', s[i])
+
+print("Slicing of string from index 2 to 7 are:-",s[2:7])
+
+print("using slicing step:-",s[1:8:2])
+
+# negative slicing
+print("By Negative index:-",s[::-1])
+
+print("Negative index and slicing:-",s[::-1])
