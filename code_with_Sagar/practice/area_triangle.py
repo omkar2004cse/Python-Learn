5de2c3c4c6da2fee822e69fdd44f2157861f663c
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-12 22:31:11 +0530

    practice

---
+++ code_with_Sagar/practice/area_triangle.py
@@ -0,0 +1,7 @@
+# Calculate the area of triangle
+# formula is 1/2(b*h)
+
+base=float(input("Enter a Base of Triangle:-"))
+height=float(input("Enter a Height of Triangle:-"))
+area=(base*height)/2
+print("Area of triangle is",area)
