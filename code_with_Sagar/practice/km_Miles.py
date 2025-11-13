010514d8b1c04562d9724f3979ff7d71a7326010
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-13 07:27:39 +0530

    practice example 9 and 10 form programiz

---
+++ code_with_Sagar/practice/km_Miles.py
@@ -0,0 +1,4 @@
+# program of converting the Km to Miles
+km=float(input("Enter a Distance in Kilometer From:-"))
+miles=km*0.621371
+print(km,"is in Miles is:-",f"{miles:.2f}")
---
+++ code_with_Sagar/practice/temperature_conversion.py
@@ -0,0 +1,3 @@
+# convert the temperature in different from 
+c=float(input("Enter Temperature in Celsius:-"))
+print("Temperature in Faherheit is:-"f"{(c*1.8+32):.2f}")
