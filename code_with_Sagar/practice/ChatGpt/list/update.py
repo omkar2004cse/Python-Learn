49564d641c5556c0dc0e16f88b0537cb788d0e40
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-12-28 21:37:14 +0530

    practice

---
+++ code_with_Sagar/practice/ChatGpt/list/update.py
@@ -0,0 +1,19 @@
+# list update and sclicing and accessing element of List
+
+
+# acesing element of list
+# index
+l=[1,2,3,4,5,"omkar",True,12.44]
+# print(l[5])
+
+# by sclicing
+# print(l[1:4])
+
+# updating
+# l[4]=56
+# print(l)
+
+
+# updating list by slicing is
+l[1:3]=12,32
+print(l)
